37038601cb19160cf3c879e4dd61a88955aac269
EDDIE05a <eddaarenas@soy.sena.edu.co>
2025-04-10 02:22:23 +0000

    Se agrega carpeta para practicar ejercicios en Python

---
+++ ConceptosPython/Varibles_Y_Condicionales.py
@@ -0,0 +1,9 @@
+#VARIABLES
+# En Python no es necesario declarar las varibales, con asignarle el nombre y el valor a la variable es suficiente.
+
+nombre = "Ana"         # str
+edad = 25              # int
+altura = 1.65          # float
+es_estudiante = True   # bool
+
+
