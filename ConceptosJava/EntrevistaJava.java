f1f28e2214be59423db9713cbe8b38dbc2faa74a
EDDIE05a <eddaarenas@soy.sena.edu.co>
2025-04-10 00:09:38 +0000

    Se agrega carpeta de conceptos sobre java

---
+++ ConceptosJava/EntrevistaJava.java
@@ -0,0 +1,13 @@
+// NUMERO PAR O INPAR
+
+ public class Animal {
+ 
+    String nombre;
+
+    public static hacerSonido(){
+        System.out.println("El animal hace un sonido");
+    }
+ }
+
+
+ 
