470d723c4ef7231a6689d2c6ea76f84a9c88d047
EDDIE05a <eddaarenas@soy.sena.edu.co>
2025-04-16 13:53:42 +0000

    Se agrega archivo MatricesVectores.js

---
+++ ConceptosJavascript/ MatricesVectores.js
@@ -0,0 +1,13 @@
+// Vector
+
+let Vector = [1, 2, 3, 4, 5]
+
+// Matriz
+
+let Matriz = [
+    [0, 1],
+    [2, 3],
+    [4, 5]
+];
+
+console.log(Matriz[1][0]); // 2 (segunda fila, primera columna)
