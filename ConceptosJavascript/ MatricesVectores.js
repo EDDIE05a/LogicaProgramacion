61c8f5176ab5dfb65248ff5b6e7e56bbed4977b4
EDDIE05a <eddaarenas@soy.sena.edu.co>
2025-04-20 21:22:26 +0000

    Se desarrollan ejercicios con matrices con uso de for y foreach

--- ConceptosJavascript/ MatricesVectores.js
+++ ConceptosJavascript/ MatricesVectores.js
@@ -13,7 +13,7 @@ let Matriz = [
 console.log(Matriz[1][0]); // 2 (segunda fila, primera columna)
 
 
-// -----------------EJERCICIOS-----------------------------
+// ----------------- EJERCICIOS MATRICES CON FOR -----------------------------
 
 // Crea una función sumarVector(vector) que reciba un array y retorne la suma de todos sus elementos.
 
@@ -96,3 +96,108 @@ function mayorEnMatriz(matriz){
     }
     return numMayor;
 }
+
+// Crea una función copiarMatriz(matriz) que devuelva una nueva matriz idéntica sin modificar la original.
+
+// function copiarMatriz(matriz){
+//     let nuevaMatriz = [];
+
+//     for(let i = 0; i < matriz.length; i++){
+//         for(let j = 0; j < matriz[i].length; j++){
+//             nuevaMatriz.push(matriz[i][j]);
+//         }
+//     }
+//     return nuevaMatriz;
+// }
+
+// Crea una función que reciba una matriz y muestre todos los números que contiene, uno por uno.
+
+function impresionFor(matriz){
+    for(let i = 0; i < matriz.length; i++){
+        for(let j = 0; j < matriz[i].length; j++){
+            console.log(matriz[i][j]);
+        }
+    }
+}
+
+function impresionForEach(matriz){
+    matriz.forEach((fila) => {
+        // Recorremos cada columna de la fila
+        fila.forEach((valor) => {
+          console.log(valor);
+        });
+    });
+}
+
+
+
+// Crea una función que cuente cuántos números hay en total dentro de una matriz.
+
+function totalNumerosFor(matriz){
+    let cont = 0;
+    for(let i = 0; i < matriz.length; i++){
+        for(let j = 0; j < matriz[i].length; j++){
+            cont++;
+        }
+    }
+    return cont; 
+}
+
+function totalNumerosForEach(matriz){
+    let cont = 0;
+    matriz.forEach((fila) => {
+        fila.forEach((valor) => {
+            cont++;
+        })
+    })
+    return cont;
+}
+
+
+// Crea una función que devuelva una nueva matriz con los mismos valores, sin modificar la original.
+
+function copiaMatrizFor(matriz){
+    let nuevaMatriz = [];
+    for(let i = 0; i < matriz.length; i++){
+        let filaCopiada = matriz[i].slice(); // ← copia cada fila
+        nuevaMatriz.push(filaCopiada);       // ← la agrega a la nueva matriz
+    }
+    return nuevaMatriz;
+}
+
+function copiarMatrizForEach(matriz){
+    let nuevaMatriz = [];
+    matriz.forEach((fila) => 
+        {
+            let filaCopiada = fila.slice(); 
+            nuevaMatriz.push(filaCopiada);  
+        }
+    )
+    return nuevaMatriz;
+}
+
+
+// Crea una función que reciba una matriz y devuelva la suma de todos sus elementos.
+
+function sumarMatrizFor(matriz){
+    let suma = 0;
+    for(let i = 0; i < matriz.length; i++){
+        for(let j = 0; j < matriz[i].length; j++){
+            suma += matriz[i][j];
+        }
+    }
+    return suma;
+}
+
+function sumarMatrizForEach(matriz){
+    let suma = 0;
+    matriz.forEach((fila) => {
+        fila.forEach((columna) =>{
+            suma += columna;
+        })
+    })
+    return suma;
+}
+
+
+
